aa547b913d4e1a355aab3782f258fff0b437cda5
Jashwanth Roy Karnati <jroy.karnati@gmail.com>
2025-12-11 21:30:31 +0530

    Start of StreamMind-AI

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+        <Header/>
+        <div>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/29b665f6-0a62-4745-b9c2-f617fb7eadc6/web/IN-en-20251208-TRIFECTA-perspective_c78aea89-8f13-4e2c-ba7a-f9b40f53bf8c_medium.jpg" alt="bg"/>
+        </div>
+    </div>
+  )
+}
+
+export default Login
